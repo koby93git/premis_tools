d7749cfe6725ab5cb2140b30d3c76d967c2f03f7
Kobylka Jakub <jakub.kobylka@skodagroup.com>
2023-02-17 08:33:47 +0100

    added exit feature

--- CC_error_fix/Program.cs
+++ CC_error_fix/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-
+using System;
 using System.Text.RegularExpressions;
 
 class Program
@@ -8,22 +8,49 @@ class Program
     {
         try
         {
+            System.Console.WriteLine("Script to fix CC errors, that CC can't fix itself");
+            System.Console.WriteLine();
+
             Function fnc = new Function();
             string path = fnc.GetLaunchPath(); 
             //System.Console.WriteLine(path);
             string pvdb = path + "pvu-app-pvdb.cfg";
             string mainCFG = path + "pvu-app.cfg";
             string pss = path + "pss-config.xml";
-
-            System.Console.WriteLine("Script to fix CC errors, that CC can't fix itself");
-            System.Console.WriteLine();
-
-            fnc.PSSCorrection(pss);
-            fnc.PVDBCorrection(pvdb);
-            fnc.MainCFGCorrection(mainCFG);
+            if(File.Exists(pvdb) && File.Exists(mainCFG) && File.Exists(pss))
+            {
+                fnc.PSSCorrection(pss);
+                fnc.PVDBCorrection(pvdb);
+                fnc.MainCFGCorrection(mainCFG);
                         
-            System.Console.WriteLine("Don't forget to increase the cfg version in Consist Configurator");
-            System.Console.ReadKey();
+                System.Console.WriteLine("Success, don't forget to increase the cfg version in Consist Configurator");
+                System.Console.ReadKey();
+            }
+            else
+            {
+                while(!(File.Exists(pvdb) && File.Exists(mainCFG) && File.Exists(pss)))
+                {
+                    System.Console.Write("Files not found, enter path to files or * to exit: ");
+                    path = System.Console.ReadLine();
+                    if(String.Equals(path, "*"))
+                    {
+                        System.Environment.Exit(0);
+                    }
+                    else 
+                    {
+                        System.Console.WriteLine();
+                        pvdb = path + "pvu-app-pvdb.cfg";
+                        mainCFG = path + "pvu-app.cfg";
+                        pss = path + "pss-config.xml";
+                    }
+                }
+                fnc.PSSCorrection(pss);
+                fnc.PVDBCorrection(pvdb);
+                fnc.MainCFGCorrection(mainCFG);
+                        
+                System.Console.WriteLine("Success, don't forget to increase the cfg version in Consist Configurator");
+                System.Console.ReadKey();
+            }            
         }
         catch(Exception e)
         {
